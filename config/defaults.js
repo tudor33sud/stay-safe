69d80668bc2c39f7fc9be8b1a4cba2b8b8f355c9
Tudor <tudor.suditu@yahoo.com>
2018-02-10 22:07:47 +0200

    added configuration management

---
+++ config/defaults.js
@@ -0,0 +1,9 @@
+const environment = process.env.NODE_ENV || 'localhost';
+module.exports = {
+    port: process.env.PORT || 3005,
+    tracingHeaderKey: process.env.TRACING_HEADER_KEY || 'x-ss-request-id',
+    KC_CLIENT_ID: process.env.KC_CLIENT_ID,
+    KC_SERVER_URL: process.env.KC_SERVER_URL,
+    KC_REALM: process.env.KC_REALM,
+    SERVICE_NAME: 'api-gateway'
+};
