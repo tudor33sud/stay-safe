9d331e59fe5896f94ade2c46bd3aae6882b22e25
Tudor <tudor.suditu@yahoo.com>
2018-02-12 23:40:29 +0200

    added data service url

--- config/defaults.js
+++ config/defaults.js
@@ -3,6 +3,7 @@ module.exports = {
     port: process.env.PORT || 3005,
     tracingHeaderKey: process.env.TRACING_HEADER_KEY || 'x-ss-request-id',
     jwtHeaderKey: process.env.JWT_HEADER_KEY || 'x-ss-jwt',
+    dataServiceUrl: process.env.DATA_SERVICE_URL || `http://localhost:3007`,
     kcClientId: process.env.KC_CLIENT_ID,
     kcServerUrl: process.env.KC_SERVER_URL,
     kcRealm: process.env.KC_REALM,
