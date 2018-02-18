fed1d24637f186c890aeacebd5c09c59ac6250b1
Tudor <tudor.suditu@yahoo.com>
2018-02-18 14:10:36 +0200

    exposed events endpoints

---
+++ controller/events.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const appConfig = require('../config');
+const security = require('../security');
+const reqUtils = require('../utils/requests');
+const request = require('request');
+
+const dataServiceRequest = request.defaults({
+    baseUrl: appConfig.dataServiceUrl,
+    json: true
+});
+
+
+router.get('/', [security.guards.authenticated], reqUtils.defaultProxyHandler(dataServiceRequest));
+
+router.post('/', [security.guards.authenticated], reqUtils.defaultProxyHandler(dataServiceRequest));
+
+router.delete('/:eventId', [security.guards.authenticated], reqUtils.defaultProxyHandler(dataServiceRequest));
+
+module.exports = router;
